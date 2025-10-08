4fda34ad6aeab8b95bae0e24b26dfc435d099d90
Fernando Levin <flevin58@gmail.com>
2025-10-09 01:59:18 +0200

    Added ohmyposh and refactored

---
+++ zsh/alias-zig.zsh
@@ -0,0 +1,5 @@
+# Add zig-related aliases
+alias gozig="cd $PROJECTS/Zig"
+alias zbr="zig build run"
+alias zbt="zig build test --summary all"
+alias zfr="zig fetch --save https://github.com/raysan5/raylib/archive/refs/tags/5.5.tar.gz"
